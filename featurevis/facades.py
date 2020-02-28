e4ca8bf6560170ffbc6c38b0b8d7f0a3003284e7
christoph <chris24.blessing@gmail.com>
2020-02-28 02:54:49 +0100

    Implement MEI facade

--- featurevis/facades.py
+++ featurevis/facades.py
@@ -29,3 +29,20 @@ class TrainedEnsembleModelFacade:
 
     def insert_members(self, members):
         self.member_table().insert(members)
+
+
+class MEIFacade:
+    def __init__(self, mei_table, method_table, trained_model_table, selector_table):
+        self.mei_table = mei_table
+        self.method_table = method_table
+        self.trained_model_table = trained_model_table
+        self.selector_table = selector_table
+
+    def get_output_selected_model(self, model, key):
+        return self.selector_table().get_output_selected_model(model, key)
+
+    def generate_mei(self, dataloaders, output_selected_model, key):
+        return self.method_table().generate_mei(dataloaders, output_selected_model, key)
+
+    def insert_mei(self, mei):
+        self.mei_table().insert1(mei)
