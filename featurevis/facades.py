7288c876788f2ca7ea00f16bb8e6d3a80e8de9c8
christoph <chris24.blessing@gmail.com>
2020-03-02 19:50:14 +0100

    Remove old MEI handler and corresponding facade

--- featurevis/facades.py
+++ featurevis/facades.py
@@ -7,20 +7,3 @@ class MEIMethodFacade:
 
     def fetch_method(self, key):
         return (self.method_table() & key).fetch1("method_fn", "method_config")
-
-
-class MEIFacade:
-    def __init__(self, mei_table, method_table, trained_model_table, selector_table):
-        self.mei_table = mei_table
-        self.method_table = method_table
-        self.trained_model_table = trained_model_table
-        self.selector_table = selector_table
-
-    def get_output_selected_model(self, model, key):
-        return self.selector_table().get_output_selected_model(model, key)
-
-    def generate_mei(self, dataloaders, output_selected_model, key):
-        return self.method_table().generate_mei(dataloaders, output_selected_model, key)
-
-    def insert_mei(self, mei):
-        self.mei_table().insert1(mei)
--- featurevis/handlers.py
+++ featurevis/handlers.py
@@ -1,8 +1,3 @@
-import tempfile
-import os
-
-import torch
-
 from nnfabrik.utility.dj_helpers import make_hash
 from . import integration
 
@@ -35,25 +30,3 @@ class MEIMethodHandler:
         method_fn = import_func(method_fn)
         mei, evaluations = method_fn(dataloader, model, method_config)
         return dict(key, evaluations=evaluations, mei=mei)
-
-
-class MEIHandler:
-    def __init__(self, mei_facade, model_loader=integration.ModelLoader, cache_size_limit=10):
-        self.mei_facade = mei_facade
-        self.model_loader = model_loader(mei_facade.trained_model_table, cache_size_limit=cache_size_limit)
-
-    def make(self, key, save_func=torch.save, temp_dir_func=tempfile.TemporaryDirectory):
-        dataloaders, model = self.model_loader.load(key=key)
-        output_selected_model = self.mei_facade.get_output_selected_model(model, key)
-        mei_entity = self.mei_facade.generate_mei(dataloaders, output_selected_model, key)
-        self._insert_mei(mei_entity, save_func, temp_dir_func)
-
-    def _insert_mei(self, mei_entity, save_func, temp_dir_func):
-        """Saves the MEI to a temporary directory and inserts the prepared entity into the table."""
-        mei = mei_entity.pop("mei").squeeze()
-        filename = make_hash(mei_entity) + ".pth.tar"
-        with temp_dir_func() as temp_dir:
-            filepath = os.path.join(temp_dir, filename)
-            save_func(mei, filepath)
-            mei_entity["mei"] = filepath
-            self.mei_facade.insert_mei(mei_entity)
