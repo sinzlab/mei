fc06f212762cfddafbabd3914d8360fe46722445
christoph <chris24.blessing@gmail.com>
2020-03-03 02:59:11 +0100

    Remove MEI method handler, tests and facade

--- featurevis/facades.py
+++
@@ -1,9 +0,0 @@
-class MEIMethodFacade:
-    def __init__(self, method_table):
-        self.method_table = method_table
-
-    def insert_method(self, method):
-        self.method_table().insert1(method)
-
-    def fetch_method(self, key):
-        return (self.method_table() & key).fetch1("method_fn", "method_config")
--- featurevis/handlers.py
+++ featurevis/handlers.py
@@ -1,4 +1,3 @@
-from nnfabrik.utility.dj_helpers import make_hash
 from . import integration
 
 
@@ -14,19 +13,3 @@ class CSRFV1SelectorHandler:
     def get_output_selected_model(self, model, key):
         neuron_pos, session_id = (self.table & key).fetch1("neuron_position", "session_id")
         return integration.get_output_selected_model(neuron_pos, session_id, model)
-
-
-class MEIMethodHandler:
-    def __init__(self, mei_method_facade):
-        self.mei_method_facade = mei_method_facade
-
-    def add_method(self, method_fn, method_config):
-        self.mei_method_facade.insert_method(
-            dict(method_fn=method_fn, method_hash=make_hash(method_config), method_config=method_config)
-        )
-
-    def generate_mei(self, dataloader, model, key, import_func=integration.import_module):
-        method_fn, method_config = self.mei_method_facade.fetch_method(key)
-        method_fn = import_func(method_fn)
-        mei, evaluations = method_fn(dataloader, model, method_config)
-        return dict(key, evaluations=evaluations, mei=mei)
