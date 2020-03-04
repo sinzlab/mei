4ee05de720aee5e832a26ec80f34afa3ba41984a
christoph <chris24.blessing@gmail.com>
2020-03-04 01:19:58 +0100

    Test if model and initial guess are transferred to CUDA if flag is set

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -31,10 +31,19 @@ class TestGradientAscent:
         return MagicMock()
 
     @pytest.fixture
-    def config(self):
+    def config(self, cuda):
         return dict(
-            optim_kwargs=None, transform=None, regularization="module.function", gradient_f=None, post_update=None
+            optim_kwargs=None,
+            transform=None,
+            regularization="module.function",
+            gradient_f=None,
+            post_update=None,
+            cuda=cuda,
         )
+
+    @pytest.fixture(params=[True, False])
+    def cuda(self, request):
+        return request.param
 
     @pytest.fixture
     def set_seed(self):
@@ -49,8 +58,12 @@ class TestGradientAscent:
         return MagicMock(return_value=dict(session1=dict(inputs=(100, 10, 24, 24))))
 
     @pytest.fixture
-    def get_initial_guess(self):
-        return MagicMock(return_value="initial_guess")
+    def get_initial_guess(self, initial_guess):
+        return MagicMock(return_value=initial_guess)
+
+    @pytest.fixture
+    def initial_guess(self):
+        return MagicMock()
 
     @pytest.fixture
     def ascend(self):
@@ -68,11 +81,11 @@ class TestGradientAscent:
         gradient_ascent()
         get_initial_guess.assert_called_once_with(1, 10, 24, 24)
 
-    def test_if_ascend_is_correctly_called(self, gradient_ascent, ascend, model):
+    def test_if_ascend_is_correctly_called(self, gradient_ascent, ascend, model, initial_guess):
         gradient_ascent()
         ascend.assert_called_once_with(
             model,
-            "initial_guess",
+            initial_guess,
             optim_kwargs=dict(),
             transform=None,
             regularization="imported_function",
@@ -90,3 +103,15 @@ class TestGradientAscent:
     def test_if_eval_method_of_model_is_called(self, gradient_ascent, model):
         gradient_ascent()
         model.eval.assert_called_once_with()
+
+    def test_if_cuda_method_of_model_is_called_depending_on_cuda_flag(self, gradient_ascent, cuda, model):
+        gradient_ascent()
+        if cuda:
+            model.cuda.assert_called_once_with()
+
+    def test_if_cuda_method_of_initial_guess_is_called_depending_on_cuda_flag(
+        self, gradient_ascent, cuda, initial_guess
+    ):
+        gradient_ascent()
+        if cuda:
+            initial_guess.cuda.assert_called_once_with()
