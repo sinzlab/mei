fda73a657ce7c3e2dfbac7d089554c6668e5f90d
christoph <chris24.blessing@gmail.com>
2020-03-18 12:25:20 +0100

    Implement/Test new version of gradient ascent

---
+++ featurevis/optimization.py
@@ -0,0 +1,76 @@
+class NumIterations:
+    """Callable that stops the optimization process after a specified number of steps."""
+
+    def __init__(self, num_iterations):
+        """Initializes NumIterations.
+
+        Args:
+            num_iterations: The number of optimization steps before the process is stopped.
+        """
+        self.num_iterations = num_iterations
+        self._current_iteration = 0
+
+    def __call__(self, current_optimum):
+        """Stops the optimization process after a set number of steps by returning False."""
+        if self._current_iteration == self.num_iterations:
+            return True
+        else:
+            self._current_iteration += 1
+            return False
+
+    def __repr__(self):
+        return f"{self.__class__.__qualname__}({self.num_iterations})"
+
+
+class MEI:
+    """Wrapper around the function and the MEI tensor."""
+
+    def __init__(self, func, initial_guess):
+        """Initializes MEI.
+
+        Args:
+            func: A callable that will receive the to be optimized MEI tensor of floats as its only argument and that
+                must return a tensor containing a single float.
+            initial_guess: A tensor containing floats representing the initial guess to start the optimization process
+                from.
+        """
+        self.func = func
+        self.initial_guess = initial_guess
+        self._mei = self._initialize_mei()
+
+    def _initialize_mei(self):
+        """Detaches and clones the initial guess and enables the gradient on the cloned tensor."""
+        mei = self.initial_guess.detach().clone()
+        mei.requires_grad_()
+        return mei
+
+    def evaluate(self):
+        """Evaluates the current MEI on the callable and returns the result."""
+        return self.func(self._mei)
+
+    def __call__(self):
+        """Detaches and clones the current MEI and returns it."""
+        return self._mei.detach().clone()
+
+    def __repr__(self):
+        return f"{self.__class__.__qualname__}({self.func}, {self.initial_guess})"
+
+
+def optimize(mei, optimizer, optimized):
+    """Optimizes the input to a given function such that it maximizes said function using gradient ascent.
+
+    Args:
+        mei: An instance of the to be optimized MEI.
+        optimizer: A PyTorch-style optimizer class.
+        optimized: A callable that receives the current optimal input as its argument before each optimization step
+            and returns a boolean. The optimization process will be stopped if it returns True.
+
+    Returns:
+        A tensor of floats having the same shape as "initial_guess" representing the input that maximizes the function.
+    """
+    while not optimized(mei):
+        optimizer.zero_grad()
+        evaluation = mei.evaluate()
+        (-evaluation).backward()
+        optimizer.step()
+    return mei()
---
+++ tests/test_optimization.py
@@ -0,0 +1,174 @@
+from unittest.mock import MagicMock
+from functools import partial
+
+import pytest
+
+from featurevis import optimization
+
+
+class TestNumIterations:
+    @pytest.fixture
+    def optimized(self):
+        def _optimized(n):
+            return optimization.NumIterations(n)
+
+        return _optimized
+
+    def test_init(self, optimized):
+        assert optimized(5).num_iterations == 5
+
+    @pytest.mark.parametrize("num_iterations", [0, 1, 1000])
+    def test_call(self, optimized, num_iterations):
+        optimized = optimized(num_iterations)
+        for _ in range(num_iterations):
+            assert optimized("current_optimum") is False
+        assert optimized("current_optimum") is True
+
+    def test_repr(self, optimized):
+        assert optimized(5).__repr__() == f"NumIterations(5)"
+
+
+class TestMEI:
+    @pytest.fixture
+    def mei(self, func, initial_guess):
+        return optimization.MEI(func, initial_guess)
+
+    @pytest.fixture
+    def func(self):
+        func = MagicMock(return_value="evaluation")
+        representation = MagicMock(return_value="func")
+        func.__repr__ = representation
+        return func
+
+    @pytest.fixture
+    def initial_guess(self):
+        initial_guess = MagicMock()
+        representation = MagicMock(return_value="initial_guess")
+        initial_guess.__repr__ = representation
+        initial_guess.detach.return_value.clone.return_value.detach.return_value.clone.return_value = "cloned_mei"
+        return initial_guess
+
+    def test_if_func_gets_stored_as_instance_attribute(self, mei, func):
+        assert mei.func is func
+
+    def test_if_initial_guess_gets_stored_as_instance_attribute(self, mei, initial_guess):
+        assert mei.initial_guess is initial_guess
+
+    def test_if_initial_guess_gets_detached(self, mei, initial_guess):
+        initial_guess.detach.assert_called_once_with()
+
+    def test_if_detached_initial_guess_gets_cloned(self, mei, initial_guess):
+        initial_guess.detach.return_value.clone.assert_called_once_with()
+
+    def test_if_cloned_initial_guess_gets_grad_enabled(self, mei, initial_guess):
+        initial_guess.detach.return_value.clone.return_value.requires_grad_.assert_called_once_with()
+
+    def test_if_func_is_correctly_called(self, mei, func, initial_guess):
+        mei.evaluate()
+        func.assert_called_once_with(initial_guess.detach.return_value.clone.return_value)
+
+    def test_if_call_returns_the_correct_value(self, mei):
+        assert mei.evaluate() == "evaluation"
+
+    def test_if_mei_is_detached_when_retrieved(self, mei, initial_guess):
+        mei()
+        initial_guess.detach.return_value.clone.return_value.detach.assert_called_once_with()
+
+    def test_if_detached_mei_is_cloned_when_retrieved(self, mei, initial_guess):
+        mei()
+        initial_guess.detach.return_value.clone.return_value.detach.return_value.clone.assert_called_once_with()
+
+    def test_if_cloned_mei_is_returned_when_retrieved(self, mei, initial_guess):
+        assert mei() == "cloned_mei"
+
+    def test_repr(self, mei):
+        assert mei.__repr__() == "MEI(func, initial_guess)"
+
+
+class TestOptimize:
+    @pytest.fixture
+    def optimize(self, mei, optimizer):
+        return partial(optimization.optimize, mei, optimizer)
+
+    @pytest.fixture
+    def mei(self, evaluation):
+        mei = MagicMock(return_value="mei")
+        mei.evaluate.return_value = evaluation
+        return mei
+
+    @pytest.fixture
+    def optimizer(self):
+        return MagicMock()
+
+    @pytest.fixture
+    def optimized(self):
+        def _optimized(num_iterations=1):
+            return MagicMock(side_effect=[False for _ in range(num_iterations)] + [True])
+
+        return _optimized
+
+    @pytest.fixture
+    def evaluation(self, negated_evaluation):
+        evaluation = MagicMock(name="evaluation")
+        evaluation.__neg__.return_value = negated_evaluation
+        return evaluation
+
+    @pytest.fixture
+    def negated_evaluation(self):
+        return MagicMock(name="negated_evaluation")
+
+    @pytest.fixture(params=[0, 1, 100])
+    def num_iterations(self, request):
+        return request.param
+
+    def test_if_optimized_is_called_correctly(self, optimize, mei, optimized):
+        optimized = optimized()
+        optimize(optimized)
+        optimized.assert_called_with(mei)
+
+    def test_if_optimizer_gradient_gets_zeroed_correctly(self, optimize, optimizer, optimized):
+        optimize(optimized())
+        optimizer.zero_grad.assert_called_with()
+
+    def test_if_mei_is_evaluated_correctly(self, optimize, mei, optimized):
+        optimize(optimized())
+        mei.evaluate.assert_called_with()
+
+    def test_if_evaluation_is_negated(self, optimize, optimized, evaluation):
+        optimize(optimized())
+        evaluation.__neg__.assert_called()
+
+    def test_if_backward_is_called_correctly_on_negated_evaluation(self, optimize, optimized, negated_evaluation):
+        optimize(optimized())
+        negated_evaluation.backward.assert_called_with()
+
+    def test_if_optimizer_takes_step_correctly(self, optimize, optimizer, optimized):
+        optimize(optimized())
+        optimizer.step.assert_called_with()
+
+    def test_if_optimized_is_called_correct_number_of_times(self, optimize, optimized, num_iterations):
+        optimized = optimized(num_iterations)
+        optimize(optimized)
+        assert optimized.call_count == num_iterations + 1
+
+    def test_if_optimizer_gradient_is_zeroed_correct_number_of_times(
+        self, optimize, optimizer, optimized, num_iterations
+    ):
+        optimize(optimized(num_iterations))
+        assert optimizer.zero_grad.call_count == num_iterations
+
+    def test_if_mei_is_evaluated_correct_number_of_times(self, optimize, mei, optimized, num_iterations):
+        optimize(optimized(num_iterations))
+        assert mei.evaluate.call_count == num_iterations
+
+    def test_if_optimizer_takes_correct_number_of_steps(self, optimize, optimizer, optimized, num_iterations):
+        optimize(optimized(num_iterations))
+        assert optimizer.step.call_count == num_iterations
+
+    def test_if_mei_is_correctly_called(self, mei, optimize, optimized):
+        optimize(optimized())
+        mei.assert_called_once_with()
+
+    def test_if_mei_is_returned(self, optimize, optimized):
+        mei = optimize(optimized())
+        assert mei == "mei"
