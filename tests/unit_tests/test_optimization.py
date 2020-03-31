d2b1c960b7722a1321de6c5e3088edc9c70ee050
christoph <chris24.blessing@gmail.com>
2020-03-31 10:54:15 +0200

    Add more structure to MEI class test code

--- tests/unit_tests/test_optimization.py
+++ tests/unit_tests/test_optimization.py
@@ -47,66 +47,69 @@ class TestMEI:
     def transformed_mei(self):
         return MagicMock(name="transformed_mei")
 
-    def test_if_func_gets_stored_as_instance_attribute(self, mei, func):
-        assert mei().func is func
+    class TestInit:
+        def test_if_func_gets_stored_as_instance_attribute(self, mei, func):
+            assert mei().func is func
 
-    def test_if_initial_guess_gets_stored_as_instance_attribute(self, mei, initial_guess):
-        assert mei().initial_guess is initial_guess
+        def test_if_initial_guess_gets_stored_as_instance_attribute(self, mei, initial_guess):
+            assert mei().initial_guess is initial_guess
 
-    def test_if_optimizer_gets_stored_as_instance_attribute(self, mei, optimizer):
-        assert mei().optimizer is optimizer
+        def test_if_optimizer_gets_stored_as_instance_attribute(self, mei, optimizer):
+            assert mei().optimizer is optimizer
 
-    def test_if_transform_gets_stored_as_instance_attribute_if_provided(self, mei, transform):
-        assert mei(transform=transform).transform is transform
+        def test_if_transform_gets_stored_as_instance_attribute_if_provided(self, mei, transform):
+            assert mei(transform=transform).transform is transform
 
-    def test_if_transform_is_identity_function_if_not_provided(self, mei):
-        assert mei().transform("mei") == "mei"
+        def test_if_transform_is_identity_function_if_not_provided(self, mei):
+            assert mei().transform("mei") == "mei"
 
-    def test_if_initial_guess_gets_grad_enabled(self, mei, initial_guess):
-        mei()
-        initial_guess.requires_grad_.assert_called_once_with()
+        def test_if_initial_guess_gets_grad_enabled(self, mei, initial_guess):
+            mei()
+            initial_guess.requires_grad_.assert_called_once_with()
 
-    def test_if_optimizer_gradient_is_zeroed(self, mei, optimizer):
-        mei().step(0)
-        optimizer.zero_grad.assert_called_with()
+    class TestStep:
+        def test_if_optimizer_gradient_is_zeroed(self, mei, optimizer):
+            mei().step(0)
+            optimizer.zero_grad.assert_called_with()
 
-    def test_if_transform_is_correctly_called(self, mei, transform, initial_guess):
-        mei(transform=transform).step(0)
-        transform.assert_called_once_with(initial_guess, i_iteration=0)
+        def test_if_transform_is_correctly_called(self, mei, transform, initial_guess):
+            mei(transform=transform).step(0)
+            transform.assert_called_once_with(initial_guess, i_iteration=0)
 
-    def test_if_func_is_correctly_called(self, mei, func, transform, transformed_mei):
-        mei(transform=transform).step(0)
-        func.assert_called_once_with(transformed_mei)
+        def test_if_func_is_correctly_called(self, mei, func, transform, transformed_mei):
+            mei(transform=transform).step(0)
+            func.assert_called_once_with(transformed_mei)
 
-    def test_if_evaluation_is_negated(self, mei, evaluation):
-        mei().step(0)
-        evaluation.__neg__.assert_called_once_with()
+        def test_if_evaluation_is_negated(self, mei, evaluation):
+            mei().step(0)
+            evaluation.__neg__.assert_called_once_with()
 
-    def test_if_backward_is_called_on_negated_evaluation(self, mei, negated_evaluation):
-        mei().step(0)
-        negated_evaluation.backward.assert_called_once_with()
+        def test_if_backward_is_called_on_negated_evaluation(self, mei, negated_evaluation):
+            mei().step(0)
+            negated_evaluation.backward.assert_called_once_with()
 
-    def test_if_optimizer_takes_a_step(self, mei, optimizer):
-        mei().step(0)
-        optimizer.step.assert_called_once_with()
+        def test_if_optimizer_takes_a_step(self, mei, optimizer):
+            mei().step(0)
+            optimizer.step.assert_called_once_with()
 
-    def test_if_evaluate_returns_the_correct_value(self, mei, evaluation):
-        assert mei().step(0) == evaluation
+        def test_if_step_returns_the_correct_value(self, mei, evaluation):
+            assert mei().step(0) == evaluation
 
-    def test_if_mei_is_detached_when_retrieved(self, mei, initial_guess):
-        mei().get_mei()
-        initial_guess.detach.assert_called_once_with()
+    class TestGetMEI:
+        def test_if_mei_is_detached_when_retrieved(self, mei, initial_guess):
+            mei().get_mei()
+            initial_guess.detach.assert_called_once_with()
 
-    def test_if_cloned_mei_is_squeezed_when_retrieved(self, mei, initial_guess):
-        mei().get_mei()
-        initial_guess.detach.return_value.squeeze.assert_called_once_with()
+        def test_if_cloned_mei_is_squeezed_when_retrieved(self, mei, initial_guess):
+            mei().get_mei()
+            initial_guess.detach.return_value.squeeze.assert_called_once_with()
 
-    def test_if_squeezed_mei_is_switched_to_cpu_when_retrieved(self, mei, initial_guess):
-        mei().get_mei()
-        initial_guess.detach.return_value.squeeze.return_value.cpu.assert_called_once_with()
+        def test_if_squeezed_mei_is_switched_to_cpu_when_retrieved(self, mei, initial_guess):
+            mei().get_mei()
+            initial_guess.detach.return_value.squeeze.return_value.cpu.assert_called_once_with()
 
-    def test_if_cloned_mei_is_returned_when_retrieved(self, mei, initial_guess):
-        assert mei().get_mei() == "final_mei"
+        def test_if_cloned_mei_is_returned_when_retrieved(self, mei, initial_guess):
+            assert mei().get_mei() == "final_mei"
 
     def test_repr(self, mei):
         assert mei().__repr__() == "MEI(func, initial_guess)"
