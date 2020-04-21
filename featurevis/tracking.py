fe4fd0f9bd0bc168978daf70ed5c2574d9845893
christoph <chris24.blessing@gmail.com>
2020-04-21 21:46:55 +0200

    Implement/Test Tracker

---
+++ featurevis/tracking.py
@@ -0,0 +1,25 @@
+from typing import Callable, Mapping
+
+
+class Tracker:
+    """Tracks the MEI optimization process.
+
+    Attributes:
+        objectives: A number of objectives to be tracked. The must be passed as keyword arguments and they must be
+            callable.
+        log: A dictionary where each key is the name of a tracked objective and the values are lists containing the data
+            returned by the corresponding objective across time.
+    """
+
+    def __init__(self, **objectives: Callable):
+        """Initializes Tracker."""
+        self.objectives = objectives
+        self.log = {n: list() for n in objectives}
+
+    def track(self, current_state: Mapping) -> None:
+        """Passes the current state of the MEI optimization process to each objective and logs the result."""
+        for name, objective in self.objectives.items():
+            self.log[name].append(objective(current_state))
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__qualname__}({', '.join(self.objectives)})"
---
+++ tests/unit_tests/test_tracking.py
@@ -0,0 +1,40 @@
+from unittest.mock import MagicMock, call
+
+import pytest
+
+from featurevis import tracking
+
+
+class TestTracker:
+    @pytest.fixture
+    def tracker(self, objectives):
+        return tracking.Tracker(**objectives)
+
+    @pytest.fixture(params=[0, 1, 10])
+    def objectives(self, request):
+        return {
+            "obj" + str(i): MagicMock(name=f"obj{i}", side_effect=lambda c_s: f"data{c_s.i_iter}")
+            for i in range(request.param)
+        }
+
+    @pytest.fixture(params=[0, 1, 10])
+    def states(self, request):
+        return tuple(MagicMock(name="current_state", i_iter=i) for i in range(request.param))
+
+    def test_init(self, tracker, objectives):
+        assert tracker.objectives == objectives
+
+    def test_if_track_calls_objectives_correctly(self, tracker, objectives, states):
+        for current_state in states:
+            tracker.track(current_state)
+        calls = [call(c_s) for c_s in states]
+        assert all(obj.mock_calls == calls for obj in objectives.values())
+
+    def test_if_objectives_are_correctly_represented_in_log(self, tracker, objectives, states):
+        for current_state in states:
+            tracker.track(current_state)
+        log = {n: ["data" + str(s.i_iter) for s in states] for n in objectives}
+        assert tracker.log == log
+
+    def test_repr(self, tracker, objectives):
+        assert tracker.__repr__() == f"Tracker({', '.join(objectives)})"
