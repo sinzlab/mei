0383cf3b049745ba477ec97933430da64c5d7110
christoph <chris24.blessing@gmail.com>
2020-04-30 17:27:14 +0200

    Remove old "gradient_ascent" method

--- featurevis/methods.py
+++ featurevis/methods.py
@@ -4,89 +4,11 @@ import torch
 from torch import Tensor
 from torch.nn import Module
 
-from nnfabrik.utility.nnf_helper import split_module_name, dynamic_import
 from nnfabrik.utility.nn_helpers import get_dims_for_loader_dict
-from . import core
 from . import optimization
 from .import_helpers import import_object
 from .domain import Input
 from .tracking import Tracker
-
-
-def import_path(path):
-    return dynamic_import(*split_module_name(path))
-
-
-def ascend(model, initial_guess, config, ascending_func=core.gradient_ascent):
-    """Wrapper around original gradient ascent used to package up the returned result."""
-    mei, function_evaluations, regularization_terms = ascending_func(model, initial_guess, **config)
-    output = dict(function_evaluations=function_evaluations)
-    if isinstance(mei, list):
-        mei = [m.cpu().squeeze() for m in mei]
-        output["progression"] = mei
-        mei = mei[-1]
-    else:
-        mei = mei.cpu().squeeze()
-    if regularization_terms:
-        output["regularization_terms"] = regularization_terms
-    return mei, function_evaluations[-1], output
-
-
-def gradient_ascent(
-    dataloaders,
-    model,
-    config,
-    seed,
-    set_seed=torch.manual_seed,
-    import_object=import_path,
-    get_dims=get_dims_for_loader_dict,
-    get_initial_guess=torch.randn,
-    ascend_func=ascend,
-):
-    """Generates MEIs.
-
-    Args:
-        dataloaders: NNFabrik style dataloader dictionary.
-        model: Callable object that returns a single number.
-        config: Dictionary of arguments for the gradient ascent function.
-        seed: Integer used to make the MEI generation process reproducible.
-        set_seed: Function used to set the seed. For testing purposes.
-        import_object: Function used to import functions given a path as a string. For testing purposes.
-        get_dims: Function used to get the input and output dimensions for all dataloaders. For testing purposes.
-        get_initial_guess: Function used to get the initial random guess for the gradient ascent. For testing purposes.
-        ascend_func: Function used to do the actual ascending. For testing purposes.
-
-    Returns:
-        The MEI as a tensor and a list of model evaluations at each step of the gradient ascent process.
-    """
-
-    set_seed(seed)
-    model.eval()
-    config = prepare_config(config, import_object)
-    mei_shape = get_input_dimensions(dataloaders, get_dims)
-    device = config.pop("device")
-    initial_guess = get_initial_guess(1, *mei_shape[1:], device=device)
-    model.to(device)
-    return ascend_func(model, initial_guess, config)
-
-
-def prepare_config(config, import_object):
-    config = prepare_optim_kwargs(config)
-    return import_functions(config, import_object)
-
-
-def prepare_optim_kwargs(config):
-    if not config["optim_kwargs"]:
-        config["optim_kwargs"] = dict()
-    return config
-
-
-def import_functions(config, import_object):
-    for attribute in ("transform", "regularization", "gradient_f", "post_update"):
-        if not config[attribute]:
-            continue
-        config[attribute] = import_object(config[attribute])
-    return config
 
 
 def get_input_dimensions(dataloaders, get_dims):
