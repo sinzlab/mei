b3a14d2408f12de9c0745c47d468058ebe4cfc79
christoph <chris24.blessing@gmail.com>
2020-05-20 14:08:17 +0200

    Restructure fixtures

--- tests/unit_tests/mixins/test_trained_ensemble_model_template_mixin.py
+++ tests/unit_tests/mixins/test_trained_ensemble_model_template_mixin.py
@@ -6,84 +6,59 @@ import pytest
 from featurevis import mixins
 
 
-@pytest.fixture
-def key():
-    return "key"
-
-
 @contextmanager
 def does_not_raise():
     yield
 
 
 @pytest.fixture
-def trained_ensemble_model_template(
-    member_table, dataset_table, trained_model_table, ensemble_model_class, insert1, fetch1, magic_and
-):
+def trained_ensemble_model_template(member_table, trained_model_table):
     trained_ensemble_model_template = mixins.TrainedEnsembleModelTemplateMixin
     trained_ensemble_model_template.Member = member_table
-    trained_ensemble_model_template.dataset_table = dataset_table
     trained_ensemble_model_template.trained_model_table = trained_model_table
-    trained_ensemble_model_template.ensemble_model_class = ensemble_model_class
-    trained_ensemble_model_template.insert1 = insert1
-    trained_ensemble_model_template.fetch1 = fetch1
-    trained_ensemble_model_template.__and__ = magic_and
     return trained_ensemble_model_template
 
 
 @pytest.fixture
 def member_table():
-    member_table = MagicMock(name="member_table", spec=mixins.TrainedEnsembleModelTemplateMixin.Member)
-    member_table.return_value.__and__.return_value.fetch = MagicMock(
-        name="member_and", return_value=[dict(m=0, a=0), dict(m=1, a=1)]
-    )
-    return member_table
-
-
-@pytest.fixture
-def dataset_table():
-    dataset_table = MagicMock()
-    dataset_table.return_value.__and__.return_value.__len__.return_value = 1
-    dataset_table.return_value.proj.return_value.__and__.return_value.fetch1.return_value = dict(ds=0)
-    return dataset_table
+    return MagicMock(name="member_table", spec=mixins.TrainedEnsembleModelTemplateMixin.Member)
 
 
 @pytest.fixture
 def trained_model_table():
-    trained_model_table = MagicMock()
-    trained_model_table.return_value.proj.return_value.__and__.return_value.fetch.return_value = [
-        dict(m=0),
-        dict(m=1),
-    ]
-    trained_model_table.return_value.load_model = MagicMock(
-        side_effect=[("dataloaders1", "model1"), ("dataloaders2", "model2")]
-    )
-    return trained_model_table
-
-
-@pytest.fixture()
-def ensemble_model_class():
-    return MagicMock()
-
-
-@pytest.fixture
-def insert1():
-    return MagicMock()
-
-
-@pytest.fixture
-def fetch1():
-    return MagicMock(name="fetch1", return_value="fetched_key")
-
-
-@pytest.fixture
-def magic_and():
-    magic_and = MagicMock(name="and")
-    magic_and.return_value.fetch1 = MagicMock(name="fetch1", return_value="ensemble_model_key")
-    return magic_and
+    return MagicMock(name="trained_model_table")
 
 
 class TestCreateEnsemble:
+    @pytest.fixture
+    def key(self):
+        return "key"
+
+    @pytest.fixture
+    def trained_ensemble_model_template(self, trained_ensemble_model_template, dataset_table, insert1):
+        trained_ensemble_model_template.dataset_table = dataset_table
+        trained_ensemble_model_template.insert1 = insert1
+        return trained_ensemble_model_template
+
+    @pytest.fixture
+    def trained_model_table(self, trained_model_table):
+        trained_model_table.return_value.proj.return_value.__and__.return_value.fetch.return_value = [
+            dict(m=0),
+            dict(m=1),
+        ]
+        return trained_model_table
+
+    @pytest.fixture
+    def dataset_table(self):
+        dataset_table = MagicMock()
+        dataset_table.return_value.__and__.return_value.__len__.return_value = 1
+        dataset_table.return_value.proj.return_value.__and__.return_value.fetch1.return_value = dict(ds=0)
+        return dataset_table
+
+    @pytest.fixture
+    def insert1(self):
+        return MagicMock()
+
     @pytest.mark.parametrize(
         "n_datasets,expectation",
         [(0, pytest.raises(ValueError)), (1, does_not_raise()), (2, pytest.raises(ValueError))],
@@ -126,6 +101,41 @@ class TestCreateEnsemble:
 
 
 class TestLoadModel:
+    @pytest.fixture
+    def trained_ensemble_model_template(self, trained_ensemble_model_template, ensemble_model_class, fetch1, magic_and):
+        trained_ensemble_model_template.ensemble_model_class = ensemble_model_class
+        trained_ensemble_model_template.fetch1 = fetch1
+        trained_ensemble_model_template.__and__ = magic_and
+        return trained_ensemble_model_template
+
+    @pytest.fixture
+    def member_table(self, member_table):
+        member_table.return_value.__and__.return_value.fetch = MagicMock(
+            name="member_and", return_value=[dict(m=0, a=0), dict(m=1, a=1)]
+        )
+        return member_table
+
+    @pytest.fixture
+    def trained_model_table(self, trained_model_table):
+        trained_model_table.return_value.load_model = MagicMock(
+            side_effect=[("dataloaders1", "model1"), ("dataloaders2", "model2")]
+        )
+        return trained_model_table
+
+    @pytest.fixture
+    def ensemble_model_class(self):
+        return MagicMock()
+
+    @pytest.fixture
+    def fetch1(self):
+        return MagicMock(name="fetch1", return_value="fetched_key")
+
+    @pytest.fixture
+    def magic_and(self):
+        magic_and = MagicMock(name="and")
+        magic_and.return_value.fetch1 = MagicMock(name="fetch1", return_value="ensemble_model_key")
+        return magic_and
+
     @pytest.mark.parametrize("provided_key,used_key", [("provided_key", "provided_key"), (None, "fetched_key")])
     def test_if_model_keys_are_correctly_fetched(
         self, trained_ensemble_model_template, magic_and, member_table, provided_key, used_key
