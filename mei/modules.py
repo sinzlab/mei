b9a8d4406663908507fb0d3aa1f12283ed2deb3d
KonstantinWilleke <konstantin.willeke@gmail.com>
2020-07-16 21:38:19 +0200

    Add target_fn to ConstrainedOutputModel

--- mei/modules.py
+++ mei/modules.py
@@ -49,16 +49,18 @@ class ConstrainedOutputModel(Module):
         model: A PyTorch module.
         constraint: An integer representing the index of a neuron in the model's output. Only the value corresponding
             to that index will be returned.
+        target_fn: Callable, that gets as an input the constrained output of the model.
         forward_kwargs: A dictionary containing keyword arguments that will be passed to the model every time it is
             called. Optional.
     """
 
-    def __init__(self, model: Module, constraint: int, forward_kwargs: Dict[str, Any] = None):
+    def __init__(self, model: Module, constraint: int, target_fn, forward_kwargs: Dict[str, Any] = None):
         """Initializes ConstrainedOutputModel."""
         super().__init__()
         self.model = model
         self.constraint = constraint
         self.forward_kwargs = forward_kwargs if forward_kwargs else dict()
+        self.target_fn = target_fn
 
     def __call__(self, x: Tensor, *args, **kwargs) -> Tensor:
         """Computes the constrained output of the model.
@@ -72,44 +74,7 @@ class ConstrainedOutputModel(Module):
             A tensor representing the constrained output of the model.
         """
         output = self.model(x, *args, **self.forward_kwargs, **kwargs)
-        return output[:, self.constraint]
-
-    def __repr__(self):
-        return f"{self.__class__.__qualname__}({self.model}, {self.constraint}, forward_kwargs={self.forward_kwargs})"
-
-
-class FunConstrainedOutputModel(Module):
-    """A model that has its output constrained.
-
-    Attributes:
-        model: A PyTorch module.
-        constraint: An integer representing the index of a neuron in the model's output. Only the value corresponding
-            to that index will be returned.
-        forward_kwargs: A dictionary containing keyword arguments that will be passed to the model every time it is
-            called. Optional.
-    """
-
-    def __init__(self, model: Module, constraint: int, fun, forward_kwargs: Dict[str, Any] = None):
-        """Initializes ConstrainedOutputModel."""
-        super().__init__()
-        self.model = model
-        self.constraint = constraint
-        self.forward_kwargs = forward_kwargs if forward_kwargs else dict()
-        self.some_fun = fun
-
-    def __call__(self, x: Tensor, *args, **kwargs) -> Tensor:
-        """Computes the constrained output of the model.
-
-        Args:
-            x: A tensor representing the input to the model.
-            *args: Additional arguments will be passed to the model.
-            **kwargs: Additional keyword arguments will be passed to the model.
-
-        Returns:
-            A tensor representing the constrained output of the model.
-        """
-        output = self.model(x, *args, **self.forward_kwargs, **kwargs)
-        return self.some_fun(output[:, self.constraint])
+        return self.target_fn(output[:, self.constraint])
 
     def __repr__(self):
         return f"{self.__class__.__qualname__}({self.model}, {self.constraint}, forward_kwargs={self.forward_kwargs})"
