c8e27152cc585796c57427bc3f2ba7fac960f357
Paul Pierzchlewicz <PPierzc@users.noreply.github.com>
2024-03-25 13:32:21 +0100

    WIP: Hackathon changes (#33)
    * add(docs): seperate out documentation from the main README.md
    
    * add: datajoint-free demo notebook (#28)
    
    * gradient_ascent does not require a dataloader, only the input shape of th MEI
    
    * basic postprocessing norm and clipping classes
    
    * [FIX] PNormContraintAndClip not calling itself
    
    * [FIX] Returning backwards compatilibity with dataloaders in the gradient_ascent.py
    
    * Demo notebook with V4 model for MEI generation without need of datajoint
    
    * Moved demo notebooks to new demo folder
    
    * Fixed a bug that mei functions couldn't be imported when notebook is in demo folder
    
    * added a line that will create a pretrained_models folder within the current directory to store the example model
    
    * Fixed error that would come up when pretrained_models folder already exists
    
    * [FIX] removed print statement
    
    * Made notebook more readable
    
    ---------
    
    Co-authored-by: Mvystrcilova <mvystrcilova1@seznam.cz>
    Co-authored-by: Paul Pierzchlewicz <PPierzc@users.noreply.github.com>
    
    * add(docs): rename demo notebooks to be more representative. Fixed black styling. Updated README.md.
    
    * documentation for gradient_ascent and postprocessing (#30)
    
    * gradient_ascent does not require a dataloader, only the input shape of th MEI
    
    * basic postprocessing norm and clipping classes
    
    * [FIX] PNormContraintAndClip not calling itself
    
    * [FIX] Returning backwards compatilibity with dataloaders in the gradient_ascent.py
    
    * Demo notebook with V4 model for MEI generation without need of datajoint
    
    * Moved demo notebooks to new demo folder
    
    * Fixed a bug that mei functions couldn't be imported when notebook is in demo folder
    
    * added a line that will create a pretrained_models folder within the current directory to store the example model
    
    * Fixed error that would come up when pretrained_models folder already exists
    
    * [FIX] removed print statement
    
    * Made notebook more readable
    
    * Documentation and cosmetic changes for postprocessing
    
    * Fixes and documentation for gradient_ascent
    
    ---------
    
    Co-authored-by: Mvystrcilova <mvystrcilova1@seznam.cz>
    Co-authored-by: Paul Pierzchlewicz <PPierzc@users.noreply.github.com>
    
    * Remove old notebooks (#31)
    
    * Removed old files
    
    * Remove old files
    
    * Minor fixes in documentation
    
    ---------
    
    Co-authored-by: Mvystrcilova <mvystrcilova1@seznam.cz>
    
    * create requirements.txt (#26)
    
    * add: installation guide on readme.
    
    * fix: minor change in readme.
    
    * fix: changed text from branch to version.
    
    * add: requirements.txt
    
    * move requirements from setup.py to requirements.txt
    
    * changed python and datajoin dependencies
    
    * requirement.txt now being parsed at setup.py
    
    ---------
    
    Co-authored-by: Paul Pierzchlewicz <PPierzc@users.noreply.github.com>
    
    * fix(black): fixed black styling
    
    * fix: setup.py (#34)
    
    * add: installation guide on readme.
    
    * fix: minor change in readme.
    
    * fix: changed text from branch to version.
    
    * add: requirements.txt
    
    * move requirements from setup.py to requirements.txt
    
    * changed python and datajoin dependencies
    
    * requirement.txt now being parsed at setup.py
    
    * requirements only with datajoint.
    
    * fixed requirements, i think.
    
    * fix setup.py
    
    ---------
    
    Co-authored-by: Paul Pierzchlewicz <PPierzc@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Leon Kremers <70718367+LeonKremers@users.noreply.github.com>
    Co-authored-by: Mvystrcilova <mvystrcilova1@seznam.cz>
    Co-authored-by: caio <51940926+oliveira-caio@users.noreply.github.com>

---
+++ mei/postprocessing.py
@@ -0,0 +1,87 @@
+import torch
+
+
+class PNormConstraint:
+    """
+    Enforces a p-norm constraint on a given image tensor.
+
+    This class is designed to be used as a callable for applying p-norm normalization on an tensor. It ensures
+    that the p-norm of the tensor does not exceed a specified maximum value. If the p-norm of the input tensor exceeds
+    this value, the tensor is scaled down to meet the constraint.
+
+    Attributes:
+        p (int): The order of the norm. A positive integer that determines the type of norm to apply.
+        max_value (float): The maximum allowed value for the p-norm of the image.
+
+    Methods:
+        __call__(img: torch.Tensor, iteration): Applies the p-norm constraint to the given tensor.
+
+    Parameters:
+        norm_value (float): The maximum allowed value for the p-norm of the image.
+        p (int, optional): The order of the norm. Defaults to 1, which corresponds to the L1 norm.
+
+    Returns:
+        torch.Tensor: The normalized tensor that satisfies the p-norm constraint.
+    """
+
+    def __init__(self, norm_value: float, p: int = 1):
+        self.p = p
+        self.max_value = norm_value
+
+    def __call__(self, img: torch.Tensor, iteration):
+        norm_value = torch.norm(img, self.p)
+        if norm_value > self.max_value:
+            normalized_img = img * (self.max_value / (norm_value + 1e-12))
+        else:
+            normalized_img = img
+        return normalized_img
+
+
+class PNormConstraintAndClip:
+    """
+    Enforces a p-norm constraint and pixel value clipping on a given image tensor.
+
+    This class combines p-norm normalization and pixel value clipping to ensure that the p-norm of an image tensor does
+     not exceed a specified maximum value, and that each pixel's value remains within specified bounds.
+
+    Attributes:
+        max_value (float): The maximum allowed value for the p-norm of the image.
+        max_pixel_value (float): The upper limit for pixel values in the image.
+        min_pixel_value (float): The lower limit for pixel values in the image.
+        p (int): The order of the norm, determining the type of normalization applied.
+
+    Methods:
+        __call__(img: torch.Tensor, iteration=None): Applies the p-norm constraint and clips the pixel values
+        of the image tensor. It first checks and scales the image tensor if its p-norm exceeds `max_value`.
+        Then, it clips the pixel values to ensure they stay within `[min_pixel_value, max_pixel_value]`.
+
+    Parameters:
+        norm_value (float, optional): The maximum allowed value for the p-norm of the image. Defaults to 30.0.
+        p (int, optional): The order of the norm, with `1` indicating the L1 norm. Defaults to 1.
+        max_pixel_value (float, optional): The maximum allowable pixel value in the image. Defaults to 1.0.
+        min_pixel_value (float, optional): The minimum allowable pixel value in the image. Defaults to -1.0.
+
+    Returns:
+        torch.Tensor: The normalized and clipped image tensor, conforming to the specified p-norm and
+        pixel value constraints.
+    """
+
+    def __init__(
+        self,
+        norm_value: float = 30.0,
+        p: int = 1,
+        max_pixel_value: float = 1.0,
+        min_pixel_value: float = -1.0,
+    ):
+        self.max_value = norm_value
+        self.max_pixel_value = float(max_pixel_value)
+        self.min_pixel_value = float(min_pixel_value)
+        self.p = p
+        self.p_norm_constraint = PNormConstraint(norm_value, p)
+
+    def __call__(self, img: torch.Tensor, iteration=None):
+        normalized_img = self.p_norm_constraint(img, iteration)
+        normalized_img = torch.where(normalized_img > self.max_pixel_value, self.max_pixel_value, normalized_img)
+        normalized_img = torch.where(normalized_img < self.min_pixel_value, self.min_pixel_value, normalized_img)
+
+        return normalized_img
